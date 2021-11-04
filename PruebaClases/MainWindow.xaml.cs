7bc8973d87e42426bd816cc28ca169183a7d961b
irene <irene.sanchez@iesdoctorbalmis.com>
2021-11-04 12:14:50 +0100

    Agregar archivos de proyecto.

---
+++ PruebaClases/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PruebaClases
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DataContext (tipo Obj)
+        Persona personaka = new Persona("Irene", 21);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NombreTextBox.DataContext = personaka;
+            EdadTextBox.DataContext = personaka;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(personaka.Nombre.ToString() + " "+ personaka.Edad.ToString());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            personaka.Edad++;
+        }
+    }
+}
---
+++ PruebaClases/Persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaClases
+{
+    class Persona : INotifyPropertyChanged
+    {
+        //propfull
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                _nombre = value;
+                //"Nombre" = nombre de la propiedad que está cambiando
+                NotifyPropertyChanged("Nombre");
+            }
+        }
+
+        private int _edad;
+
+        public int Edad
+        {
+            get { return _edad; }
+            set
+            {
+                _edad = value;
+                NotifyPropertyChanged("Edad");
+            }
+        }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+        public Persona()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
